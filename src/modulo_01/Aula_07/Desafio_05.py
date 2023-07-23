8758032b06930c60d819273622fd330b94f99668
GuilhermeGomesDosSantos <Gomes3654gui@gmail.com>
2023-07-23 19:32:59 -0300

    Desafio 05

---
+++ src/modulo_01/Aula_07/Desafio_05.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor
+num = int(input('Digite um Número: '))
+# print('Sucessor de {} é: {}'.format(num, num+ 1))
+# print('Antecessor de {} é {}'.format(num, num -1))
+
+print('Analisando o valor {}, seu antecessor é {} e o Sucessor é {}'.format(num, (num - 1), (num + 1)))
