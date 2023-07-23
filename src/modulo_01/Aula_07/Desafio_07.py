b14f7f38ac9c7689bfa4a7cf367a5a77b41a4d9a
GuilhermeGomesDosSantos <Gomes3654gui@gmail.com>
2023-07-23 19:35:04 -0300

    Desafio 07

---
+++ src/modulo_01/Aula_07/Desafio_07.py
@@ -0,0 +1,8 @@
+# Faça um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+n1 = int(input('Digite a 1° Nota do Aluno: '))
+n2 = int(input('Digite a 2° Nota do Aluno: '))
+
+media = (n1 + n2) / 2
+
+print('A media entre {} e {} é: {}'.format(n1, n2, media))
+
