72e0ace052220dbea5d6873c5edfb71b43fd62e0
GuilhermeGomesDosSantos <Gomes3654gui@gmail.com>
2023-07-23 07:48:03 -0300

    Desafio 01

---
+++ src/modulo_01/Aula_04_Exercicio_01.py
@@ -0,0 +1,5 @@
+#Crie um script Python que leia o nome de uma pessoa
+# e mostra uma mensagem de boas-vindas de acordo com o valor digitado
+
+nome = input('Qual é o seu nome ? ')
+print('Olá, ', nome + ' Prazer em te conhecer!');
