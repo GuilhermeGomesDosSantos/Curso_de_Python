7f7b83c30a9f96baabc2ab2e312cd0a786ab3009
GuilhermeGomesDosSantos <Gomes3654gui@gmail.com>
2023-07-23 10:13:26 -0300

    Aula 06 Testes

---
+++ src/modulo_01/Aula_06/Aula_06_Testes.py
@@ -0,0 +1,22 @@
+# n = float(input('Digite um valor: '))
+# print(n)
+
+
+# n = str(input('Digite um valor: '))
+# print(type(n))
+
+# n = bool(input('Digite um valor: '))
+# print(n)
+
+# n = input('Digite um valor: ')
+# print(n.isnumeric())
+
+
+# n = input('Digite um valor: ')
+# print(n.isalpha())
+
+# n = input('Digite um valor: ')
+# print(n.isalnum())
+
+n = input('Digite um valor: ')
+print(n.isupper())
