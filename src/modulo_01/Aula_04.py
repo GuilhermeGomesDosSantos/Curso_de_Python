c644adc765aa3590f6af35fd73b8889a31267afc
GuilhermeGomesDosSantos <Gomes3654gui@gmail.com>
2023-07-23 07:46:41 -0300

    Aula 04

---
+++ src/modulo_01/Aula_04.py
@@ -0,0 +1,17 @@
+# print('Ola, Mundo');
+# print(7 + 4);
+# print('7' + '4');
+# print('Ola' + 5);## vai dar erro
+# print('Ola', 5);
+
+# nome = 'Guilherme';
+# idade = 20;
+# peso = 95;
+# print(nome, idade, peso);
+# print(nome + idade + peso);# vai dar erro
+
+nome = input('Qual é o seu Nome ?');
+idade = input('Qual é a sua Idade ?')
+peso = input('Qual é o seu Peso ?');
+
+print(nome, idade, peso);
