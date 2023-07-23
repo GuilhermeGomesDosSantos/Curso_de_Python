ee6c55fb466cddad1cca7c749a73fd00c5143502
GuilhermeGomesDosSantos <Gomes3654gui@gmail.com>
2023-07-23 08:14:37 -0300

    Desafio 03

---
+++ src/modulo_01/Aula_04_Exercicio_03.py
@@ -0,0 +1,6 @@
+# Crie um script Python que leia dois numeros e tente mostrar a soma entre eles
+
+primeiroNumero = int(input('Primeiro Número: '))
+segundoNumero = int(input('Segundo Número: '))
+soma = primeiroNumero + segundoNumero
+print('A soma é:', soma)
