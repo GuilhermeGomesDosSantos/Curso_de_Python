6d6280a8fad771b4160867019f18b26f9bee86a2
GuilhermeGomesDosSantos <Gomes3654gui@gmail.com>
2023-07-25 21:54:19 -0300

    Aula 08

---
+++ src/modulo_01/Aula_08/Aula_08.py
@@ -0,0 +1,18 @@
+# import math
+# from math import sqrt, floor
+# num = int(input('Digite um número: '))
+# raiz = math.sqrt(num)
+# print('A raiz de {} é igual a {}'.format(num, raiz))
+# print('A raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
+# print('A raiz de {} é igual a {}'.format(num, floor(raiz)))
+#================================================================#
+
+# import random
+# num = random.random()
+# num = random.randint(1, 10)
+# print(num)
+#===============================
+
+import emoji
+print(emoji.emojize('Olá, Mundo :sunglasses:'))
+print(emoji.emojize('Olá, Mundo :earth_americas:', use_aliases=True))
