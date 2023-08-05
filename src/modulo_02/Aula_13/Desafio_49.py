f945a6a7314bdeed10cac5644f6f59aa6cabcf3e
GuilhermeGomesDosSantos <Gomes3654gui@gmail.com>
2023-08-05 13:13:54 -0300

    Desafio 49

---
+++ src/modulo_02/Aula_13/Desafio_49.py
@@ -0,0 +1,6 @@
+# Refaça o DESAFIADOR 09, mostrando a tabuada de um número que o usuário escolher.
+# só que agora utilizando um laço for.
+
+num = int(input('Digite um número ára ver sua tabuada: '))
+for c in range (1, 11):
+    print('{} X {} = {:2}'.format(num, c, num*c))
