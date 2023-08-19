124fee8319840a2eedaac5c6d8888bf2ae051fb3
GuilhermeGomesDosSantos <Gomes3654gui@gmail.com>
2023-08-19 17:02:56 -0300

    Aula 15

---
+++ src/modulo_02/Aula_15/Aula_15.py
@@ -0,0 +1,24 @@
+
+#LOOP INFINITO
+# cont = 1
+# while True:
+#     print(cont, '->',end='')
+#     cont += 1
+# print('Acabou')
+
+
+# n = 0
+# s = 0
+
+# while True:
+#     n = int(input('Digite um Número: '))
+#     if n == 999:
+#         break
+#     s += n
+# # print('A soma vale {}'.format(s))
+# print(f'A soma vale {s}')
+
+nome = 'Guilherme'
+idade = 20
+salario = 1500
+print(f'O {nome} tem {idade} anos e ganha R${salario:.2f}')
