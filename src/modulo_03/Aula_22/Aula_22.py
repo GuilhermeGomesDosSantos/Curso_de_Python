d64c2bf05fb4c4901b62e9027382f42952417976
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2023-10-07 22:51:32 -0300

    Aula 22

---
+++ src/modulo_03/Aula_22/Aula_22.py
@@ -0,0 +1,15 @@
+# import uteis
+
+# num = int (input("Digite um valor: "))
+# fat = uteis.fatorial(num)
+# print(f"O Fatorial de {num} é {fat}")
+# print(f"O Dobro de {num} é {uteis.dobro(num)}")
+# print(f"O Triplo de {num} é {uteis.triplo(num)}")
+
+# from uteis import fatorial, dobro, triplo
+
+num = int (input("Digite um valor: "))
+fat = fatorial(num)
+print(f"O Fatorial de {num} é {fat}")
+print(f"O Dobro de {num} é {dobro(num)}")
+print(f"O Triplo de {num} é {triplo(num)}")
