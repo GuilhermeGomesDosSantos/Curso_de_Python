d7999320b8300250681e26fdecca21ca9e03df34
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2023-10-14 09:46:52 -0300

    Desafio 115B

--- src/modulo_03/Aula_23/Desafio_115/systema.py
+++ src/modulo_03/Aula_23/Desafio_115/systema.py
@@ -1,10 +1,22 @@
 from lib.interface import *
+from lib.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+
+    criarArquivo(arq)
+
 
 while True:
     resposta = menu(['Ver Pessoas Cadastradas', 'Cadastrar Nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
+        # Opção de listar o coneúdo de um arquivo!
         cabecalho('Opção 1')
+
+        lerArquivo(arq)
+        
     elif resposta == 2:
         cabecalho('Opção 2')
     elif resposta == 3:
